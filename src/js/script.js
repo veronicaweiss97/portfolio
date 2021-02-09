8e17e2cbd9c2a28c04d6d604fab25b84fddeccbf
veronica <veronicaweiss77@gmail.com>
2021-02-09 14:42:53 +0100

    preloader

--- src/js/script.js
+++ src/js/script.js
@@ -92,4 +92,11 @@ window.addEventListener('scroll', () => {
     }
 });
 
-
+//preloader 
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
